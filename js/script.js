4b222a9f2e8ab21de118ed322a51a6e3eb0e57f2
maxexc <max.exc7@gmail.com>
2022-08-06 02:55:59 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -470,34 +470,3 @@ function formatMessage(message, maxLength) {
 }
 
 formatMessage("Curabitur ligula sapien", 16);
-
-function getShippingCost(country) {
-  let message;
-  // Change code below this line
-  switch (country) {
-    case "China":
-      message = `Shipping to ${country} will cost 100 credits`;
-      break;
-    case "Chile":
-      message = `Shipping to ${country} will cost 250 credits`;
-      break;
-    case "Australia":
-      message = `Shipping to ${country} will cost 170 credits`;
-      break;
-    case "Jamaica":
-      message = `Shipping to ${country} will cost 120 credits`;
-      break;
-    default:
-      message = "Sorry, there is no delivery to your country";
-      return message;
-  }
-  console.log(message);
-  // Change code above this line
-}
-
-getShippingCost("Australia");
-getShippingCost("Germany");
-getShippingCost("China");
-getShippingCost("Chile");
-getShippingCost("Jamaica");
-getShippingCost("Sweden");
