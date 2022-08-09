b07303e01743641fc35c0311e085780442810099
maxexc <max.exc7@gmail.com>
2022-08-09 19:10:09 +0300

    Update js-hw2.js

--- js/js-hw2.js
+++ js/js-hw2.js
@@ -19,3 +19,24 @@ resetBtn.addEventListener("click", function () {
   total = 0;
   outputEl.textContent = total;
 });
+
+// Array / Массивы
+
+const clients = ["Mango", "Poly", "Ajax"];
+
+// console.log(clients[0]);
+
+// clients[0] = "Kiwi";
+// clients[4] = "Pango";
+// console.log(clients[0]);
+
+// console.log(clients.length);
+// console.log(clients[3]);
+
+const lastElementIndex = clients.length - 1;
+console.log(lastElementIndex);
+console.log(clients[lastElementIndex]);
+
+for (let i = 0; i < clients.length; i += 1) {
+  console.log(clients[i]);
+}
