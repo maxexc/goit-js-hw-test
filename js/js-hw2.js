86b1b3c7fe048e53eff94ebd41524f67bf0b2724
maxexc <max.exc7@gmail.com>
2022-08-10 00:43:15 +0300

    Update js-hw2.js

--- js/js-hw2.js
+++ js/js-hw2.js
@@ -22,7 +22,7 @@ resetBtn.addEventListener("click", function () {
 
 // Array / Массивы
 
-const clients = ["Mango", "Poly", "Ajax"];
+// const clients = ["Mango", "Poly", "Ajax"];
 
 // console.log(clients[0]);
 
@@ -33,10 +33,37 @@ const clients = ["Mango", "Poly", "Ajax"];
 // console.log(clients.length);
 // console.log(clients[3]);
 
-const lastElementIndex = clients.length - 1;
-console.log(lastElementIndex);
-console.log(clients[lastElementIndex]);
+// const lastElementIndex = clients.length - 1;
+// console.log(lastElementIndex);
+// console.log(clients[lastElementIndex]);
 
-for (let i = 0; i < clients.length; i += 1) {
-  console.log(clients[i]);
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log(clients[i]);
+// }
+
+// for (const client of clients) {
+//   console.log(client);
+// }
+
+// const string = "javascript";
+
+// for (const chush of string) {
+//   console.log(chush);
+// }
+
+const clients = ["Mango", "Poly", "Ajax"];
+const clientNameToFind = "Poly";
+let message;
+
+for (const client of clients) {
+  // На каждой итерации будем проверять совпадает ли элемент массива с
+  // именем клиента. Если совпадает то мы записываем в message сообщение
+  // об успехе и делаем break чтобы не искать дальше
+  if (client === clientNameToFind) {
+    message = "Клиент с таким именем есть в базе данных!";
+    break;
+  }
+
+  // Если они не совпадают то запишем в message сообщение об отсутствии имени
+  message = "Клиента с таким именем нету в базе данных!";
 }
