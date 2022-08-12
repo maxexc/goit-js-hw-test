366963e557a36be65a5da948522c3311775efb53
maxexc <max.exc7@gmail.com>
2022-08-13 02:09:43 +0300

    Update js-hw2.js

--- js/js-hw2.js
+++ js/js-hw2.js
@@ -51,30 +51,58 @@ resetBtn.addEventListener("click", function () {
 //   console.log(chush);
 // }
 
-const clients = ["Mango", "Poly", "Ajax"];
-const clientNameToFind = "Poly";
-let message;
+// const clients = ["Mango", "Poly", "Ajax"];
+// const clientNameToFind = "Poly";
+// let message;
 
-for (const client of clients) {
-  // На каждой итерации будем проверять совпадает ли элемент массива с
-  // именем клиента. Если совпадает то мы записываем в message сообщение
-  // об успехе и делаем break чтобы не искать дальше
-  if (client === clientNameToFind) {
-    message = "Клиент с таким именем есть в базе данных!";
-    break;
-  }
+// for (const client of clients) {
+//   // На каждой итерации будем проверять совпадает ли элемент массива с
+//   // именем клиента. Если совпадает то мы записываем в message сообщение
+//   // об успехе и делаем break чтобы не искать дальше
+//   if (client === clientNameToFind) {
+//     message = "Клиент с таким именем есть в базе данных!";
+//     break;
+//   }
 
-  // Если они не совпадают то запишем в message сообщение об отсутствии имени
-  message = "Клиента с таким именем нету в базе данных!";
+//   // Если они не совпадают то запишем в message сообщение об отсутствии имени
+//   message = "Клиента с таким именем нету в базе данных!";
+// }
+
+// const name = "Mango";
+// console.log(name.split(""));
+
+// const mess = "JavaScript это интересно";
+// console.log(mess.split(" "));
+
+// const numbers = [1, 2, 3, 8];
+
+// numbers.unshift(0.5);
+// console.log(numbers); // 0.5
+
+// multiply(2, 3, 5); // Результат умножения равен 30
+// function multiply(x, y, z) {
+//   multiply = x * y * z;
+//   return;
+// }
+// console.log(multiply);
+
+// console.log("Лог после вызова функции multiply");
+
+// function multiply() {
+//   let total = 1;
+
+//   for (const dog of arguments) {
+//     total *= dog;
+//   }
+
+//   return total;
+// }
+
+// console.log(multiply(1, 2, 3, 4, 5));
+
+function multiply(...arf) {
+  // const client = Array.from(arguments);
+  return arf;
 }
 
-const name = "Mango";
-console.log(name.split(""));
-
-const mess = "JavaScript это интересно";
-console.log(mess.split(" "));
-
-const numbers = [1, 2, 3, 8];
-
-numbers.unshift(0.5);
-console.log(numbers); // 0.5
+console.log(multiply(3, 5));
