27a16011fcbaaa9690dd0835e38beff3f2b614c4
maxexc <max.exc7@gmail.com>
2022-08-21 02:46:50 +0300

    up

--- js/js-hw2next.js
+++ js/js-hw2next.js
@@ -147,5 +147,22 @@ console.log(dog.hasOwnProperty("name")); // true
 console.log(dog.hasOwnProperty("tails")); // false
 
 //
-const temps = [14, -4, 25, 8, 11];
-console.log(Math.max(...temps)); // 25
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4.7,
+  price: 5000,
+  tags: ["premium", "promoted", "top", "trusted"],
+  owner: {
+    name: "Henry Sibola",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+// Change code below this line
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location = { country: "Jamaica", city: "Kingston" };
+
+console.log(apartment.location.country);
---
+++ js/js-hw3.js
@@ -0,0 +1,11 @@
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const keys = Object.keys(apartment);
+console.log(keys);
+const values = Object.values(apartment);
+console.log(values);
+
+// Change code below this line
