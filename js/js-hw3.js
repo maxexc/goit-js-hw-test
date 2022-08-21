a4a61de4c9541db6269fb643a0b3e0908c6a89a6
maxexc <max.exc7@gmail.com>
2022-08-21 13:41:23 +0300

    Update js-hw3.js

--- js/js-hw3.js
+++ js/js-hw3.js
@@ -9,3 +9,32 @@ const values = Object.values(apartment);
 console.log(values);
 
 // Change code below this line
+
+// function countProps(object) {
+//   let propCount = 0;
+//   // Change code below this line
+//   // const arrayObject = Object.entries(object);
+
+//   propCount = Object.entries(object).length;
+//   // Change code above this line
+//   console.log(propCount);
+//   return propCount;
+// }
+
+// countProps({ name: "Mango", age: 2 });
+// countProps({});
+// countProps({ mail: "poly@mail.com", isOnline: true, score: 500 });
+
+function countProps(object) {
+  // Change code below this line
+  let propCount = 0;
+  const keys = Object.keys(object);
+
+  for (const key of keys) {
+    propCount += 1;
+  }
+  console.log(propCount);
+  return propCount;
+  // Change code above this line
+}
+countProps({ name: "Mango", age: 2 });
