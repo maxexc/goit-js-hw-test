f86cf5ea830e4b624a47b97e4fe66cad1dbcd779
maxexc <max.exc7@gmail.com>
2022-08-26 17:23:11 +0300

    Update js-hw4.js

---
+++ js/js-hw4.js
@@ -0,0 +1,58 @@
+// Колбэк-функция
+function greet(name) {
+  console.log(`Добро пожаловать ${name}.`);
+}
+
+// Функция высшего порядка
+function registerGuest(name, callback) {
+  console.log(`Регистрируем гостя ${name}.`);
+  callback(name);
+}
+
+registerGuest("Манго", greet);
+
+//
+function processCall(recipient) {
+  // Имитируем доступность абонента случайным числом
+  const isRecipientAvailable = Math.random() > 0.5;
+
+  if (!isRecipientAvailable) {
+    console.log(`Абонент ${recipient} недоступен, оставьте сообщение.`);
+    // Логика активации автоответчика
+  } else {
+    console.log(`Соединяем с ${recipient}, ожидайте...`);
+    // Логика принятия звонка
+  }
+}
+
+processCall("Манго");
+
+// function processCall(recipient, onAvailable, onNotAvailable) {
+//   // Имитируем доступность абонента случайным числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     onNotAvailable(recipient);
+//     return;
+//   }
+
+//   onAvailable(recipient);
+// }
+
+// function takeCall(name) {
+//   console.log(`Соединяем с ${name}, ожидайте...`);
+//   // Логика принятия звонка
+// }
+
+// function activateAnsweringMachine(name) {
+//   console.log(`Абонент ${name} недоступен, оставьте сообщение.`);
+//   // Логика активации автоответчика
+// }
+
+// function leaveHoloMessage(name) {
+//   console.log(`Абонент ${name} недоступен, записываем голограмму.`);
+//   // Логика записи голограммы
+// }
+
+// processCall("Манго", takeCall, activateAnsweringMachine);
+// processCall("Поли", takeCall, leaveHoloMessage);
