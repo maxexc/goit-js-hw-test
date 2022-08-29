3e01ca5fce773ea66306fe9f1b5246413d0b18e3
maxexc <max.exc7@gmail.com>
2022-08-29 23:36:05 +0300

    Update js-new.js

--- js/js-new.js
+++ js/js-new.js
@@ -13,11 +13,11 @@ console.log("doubledNums", doubledNums);
 
 //
 const players = [
-  { id: "player-1", name: "Mango", timePlayed: 310, point: 54, online: false },
-  { id: "player-2", name: "Poly", timePlayed: 470, point: 92, online: true },
-  { id: "player-3", name: "Kiwi", timePlayed: 230, point: 48, online: true },
-  { id: "player-4", name: "Ajax", timePlayed: 150, point: 71, online: false },
-  { id: "player-5", name: "Chelsy", timePlayed: 80, point: 48, online: true },
+  { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
+  { id: "player-2", name: "Poly", timePlayed: 470, points: 92, online: true },
+  { id: "player-3", name: "Kiwi", timePlayed: 230, points: 48, online: true },
+  { id: "player-4", name: "Ajax", timePlayed: 150, points: 71, online: false },
+  { id: "player-5", name: "Chelsy", timePlayed: 80, points: 48, online: true },
 ];
 
 const playerNames = players.map((player) => player.name);
@@ -26,10 +26,10 @@ console.log("playerNames", playerNames);
 const playerIds = players.map((player) => player.id);
 console.log("playerIds", playerIds);
 
-const res = players.map(({ name, online }) => ({
-  name,
-  online,
-}));
+// const res = players.map(({ name, online }) => ({
+//   name,
+//   online,
+// }));
 
 // const res = players.map((player) => {
 //   return {
@@ -37,4 +37,16 @@ const res = players.map(({ name, online }) => ({
 //     online: player.online,
 //   };
 // });
-console.log("res", res);
+// console.log("res", res);
+
+const updatedPlayers = players.map((player) => {
+  console.log(player);
+
+  return {
+    ...player,
+    points: player.points * 1.1,
+  };
+});
+
+console.table(updatedPlayers);
+console.log(updatedPlayers);
