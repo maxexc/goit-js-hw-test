a11044e92e6b02bef2e9cfd28d276e2c414524d8
maxexc <max.exc7@gmail.com>
2022-09-01 00:24:51 +0300

    Update js-hw5.js

--- js/js-hw5.js
+++ js/js-hw5.js
@@ -231,3 +231,37 @@ builder2.padEnd("^");
 console.log(builder2.getValue()); // "^.^"
 builder2.padBoth("=");
 console.log(builder2.getValue()); // "=^.^="
+
+//
+class Car5 {
+  // Change code below this line
+  static MAX_PRICE = 50000;
+  #price;
+
+  constructor({ price }) {
+    this.#price = price;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    if (newPrice > Car5.MAX_PRICE) {
+      return this.#price;
+    } else if (newPrice <= Car5.MAX_PRICE) {
+      return (this.#price = newPrice);
+    }
+  }
+
+  // Change code above this line
+}
+
+const audi = new Car5({ price: 35000 });
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000
