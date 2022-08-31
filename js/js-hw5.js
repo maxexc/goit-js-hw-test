b5a45a079ea3c55622ff23177b16c92e8a906df8
maxexc <max.exc7@gmail.com>
2022-08-31 20:13:09 +0300

    up

---
+++ js/js-hw5.js
@@ -0,0 +1,82 @@
+const pizzaPalace = {
+  pizzas: ["Supercheese", "Smoked", "Four meats"],
+  // Change code below this line
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `Sorry, there is no pizza named «${pizzaName}»`;
+    }
+
+    return `Order accepted, preparing «${pizzaName}» pizza`;
+  },
+  // Change code above this line
+};
+
+pizzaPalace.order("Four meats");
+console.log(pizzaPalace.order("Four meats"));
+pizzaPalace.order("Big Mike");
+console.log(pizzaPalace.order("Big Mike"));
+
+//
+const historyService = {
+  orders: [
+    { email: "jacob@hotmail.com", dish: "Burrito" },
+    { email: "solomon@topmail.net", dish: "Burger" },
+    { email: "artemis@coldmail.net", dish: "Pizza" },
+    { email: "solomon@topmail.net", dish: "Apple pie" },
+    { email: "jacob@hotmail.com", dish: "Taco" },
+  ],
+  // Change code below this line
+  getOrdersLog() {
+    return this.orders
+      .map((order) => `email: ${order.email} dish: ${order.dish}`)
+      .join(" - ");
+  },
+  getEmails() {
+    const emails = this.orders.map((order) => order.email);
+    const uniqueEmails = new Set(emails);
+    return [...uniqueEmails];
+  },
+  getOrdersByEmail(email) {
+    return this.orders.filter((order) => order.email === email);
+  },
+  // Change code above this line
+};
+historyService.getOrdersLog();
+historyService.getEmails();
+historyService.getOrdersByEmail("solomon@topmail.net");
+console.log(historyService.getOrdersLog());
+console.log(historyService.getEmails());
+console.log(historyService.getOrdersByEmail("solomon@topmail.net"));
+
+// ПРОТОТИП ОБЪЕКТА
+//
+const parent = {
+  name: "Stacey",
+  surname: "Moore",
+  age: 54,
+  heritage: "Irish",
+};
+// Change code below this line
+
+const child = Object.create(parent);
+
+// Change code above this line
+child.name = "Jason";
+child.age = 27;
+
+child.hasOwnProperty("name");
+console.log('child.hasOwnProperty("name")', child.hasOwnProperty("name"));
+child.heritage;
+console.log(child.heritage);
+child.hasOwnProperty("heritage");
+console.log(
+  "child.hasOwnProperty(heritage):",
+  child.hasOwnProperty("heritage")
+);
+
+//
--- js/js-new.js
+++ js/js-new.js
@@ -126,3 +126,126 @@ user.showContext = showThis;
 // this будет указывать на текущий объект, в контексте
 // которого осуществляется вызов, а не на глобальный объект.
 user.showContext(); // this in showThis: {username: "Mango", showContext: ƒ}
+
+//
+const salary = {
+  mango: 100,
+  poly: 50,
+  ajax: 150,
+};
+
+console.log(Object.values(salary));
+const totalSalary = Object.values(salary).reduce((total, value) => {
+  return total + value;
+}, 0);
+const totalSalary2 = Object.values(salary).reduce((total, value) => {
+  return total + value;
+}); // "0" можно и не ставиить!
+console.log("totalSalary:", totalSalary);
+console.log("totalSalary2:", totalSalary2);
+
+//
+// const totalTimePlayed = players.reduce((totalTime, value) => {
+//   return totalTime + value.timePlayed;
+// }, 0);
+const totalTimePlayed = players.reduce(
+  (totalTime, value) => totalTime + value.timePlayed,
+  0
+);
+
+console.log("totalTimePlayed:", totalTimePlayed);
+
+//
+const cart = [
+  { label: "Apples", price: 100, quantity: 2 },
+  { label: "Bananas", price: 120, quantity: 3 },
+  { label: "Lemons", price: 70, quantity: 4 },
+];
+
+// const totalAmount = cart.reduce(
+//   (total, item) => total + item.price * item.quantity,
+//   0
+// );
+const totalAmount = cart.reduce(
+  (total, { price, quantity }) => total + price * quantity,
+  0
+);
+console.table(cart);
+console.log("totalAmount:", totalAmount);
+
+//
+const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+];
+
+// const allTags = tweets.reduce((tags, tweet) => {
+//   tags.push(...tweet.tags); // валидатор может ругатся - изменяем функцию!
+
+//   return tags;
+// }, []);
+// console.table(allTags);
+const allTags = tweets.reduce((tags, tweet) => [...tags, ...tweet.tags], []);
+console.table(tweets);
+console.log(allTags);
+
+// tags = [], tweet.tags = ['js', 'nodejs'] return [...[], ...['js', 'node.js']]
+// tags = [js', 'node.js'] tweet.tags ['html', 'css']
+//    return [...['js', 'nodejs']], ...['html', 'css']
+//    ['js', 'nodejs', 'html', 'css']
+
+// const tagsStats = allTags.reduce((acc, tag) => {
+//   console.log(acc);
+
+//   if (acc[tag]) {
+//     acc[tag] += 1;
+
+//     return acc;
+//   }
+//   acc[tag] = 1;
+//   return acc;
+// }, {});
+
+const tagsStats = allTags.reduce((acc, tag) => {
+  return {
+    ...acc,
+    [tag]: acc[tag] ? (acc[tag] += 1) : 1,
+  };
+}, {});
+console.log(tagsStats);
+
+// const tagsStats = allTags.reduce(
+//   (acc, tag) => ({
+//     ...acc,
+//     [tag]: acc[tag] ? (acc[tag] += 1) : 1,
+//   }),
+//   {}
+// );
+
+// example
+const user3 = {
+  name: "Mango",
+};
+const key = "name";
+console.log(user3[key]);
+console.log(user3["name"]);
+
+// COPY ARRAY
+const numbers3 = [1, 9, 16, 2, 3];
+console.log("numbers3", numbers3);
+
+const copy = [...numbers3];
+console.log("copy", copy);
+console.log("copy === numbers3", copy === numbers3);
+
+const numSorted = [...numbers3].sort((a, b) => a - b);
+console.log("numSorted", numSorted);
+
+//
+const sortedTimes = [...players].sort((a, b) => a.timePlayed - b.timePlayed);
+console.table(sortedTimes);
+const sortedNames = [...players].sort((a, b) => a.name.localeCompare(b.name));
+console.table(sortedNames);
