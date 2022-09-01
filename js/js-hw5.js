39d5f74adcf5bd0ed707fc9059855445cc8551b3
maxexc <max.exc7@gmail.com>
2022-09-01 23:04:23 +0300

    Update js-hw5.js

--- js/js-hw5.js
+++ js/js-hw5.js
@@ -361,3 +361,5 @@ mango.blacklist("poly@mail.com");
 console.log(mango.blacklistedEmails); // ["poly@mail.com"]
 console.log(mango.isBlacklisted("mango@mail.com")); // false
 console.log(mango.isBlacklisted("poly@mail.com")); // true
+
+//
