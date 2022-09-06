10951e8fcff092043e126c411ac874f8c4128bed
maxexc <max.exc7@gmail.com>
2022-09-06 22:59:50 +0300

    update

---
+++ js/js-hw6.js
@@ -0,0 +1,12 @@
+// console.log(window);
+
+// console.log(document);
+
+console.dir(document);
+
+// const navRef;
+// const navEl = document.querySelector("ul");
+// console.dir(navEl);
+
+const navItemEl = document.querySelector("li");
+console.dir(navItemEl);
