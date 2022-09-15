6bad5cfc17af6010ddb2ab2ee517809460f473fd
maxexc <max.exc7@gmail.com>
2022-09-15 16:43:10 +0300

    update

--- js/js-hw6.js
+++ js/js-hw6.js
@@ -1,3 +1,5 @@
+const magicBtn = document.querySelector(".js-magic-btn");
+
 // console.log(window);
 
 // console.log(document);
@@ -14,7 +16,17 @@ console.log(navItemEl);
 
 // const navLinksEl = document.querySelector(".site-nav__link3232");  // null
 const navLinksEl = document.querySelectorAll(".site-nav__link");
+// const navLinksEl = document.querySelectorAll(".site-nav .site-nav__link");
 // const navLinksEl = document.querySelectorAll(".site-nav__link121"); // []
 console.log("navLinksEl", navLinksEl);
 
+// magicBtn.addEventListener("click", () => {});
+
+magicBtn.addEventListener("click", () => {
+  const imageEl = document.querySelector(".hero__image");
+  console.log("imageEl", imageEl);
+  console.log(imageEl.src);
+  imageEl.src =
+    "https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=480";
+});
 // //
