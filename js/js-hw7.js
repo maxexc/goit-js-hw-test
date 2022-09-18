3f4db6e3dc01d7a6a0925b894fa59a8d3a1d626b
maxexc <max.exc7@gmail.com>
2022-09-18 18:19:40 +0300

    update

--- js/js-hw7.js
+++ js/js-hw7.js
@@ -41,3 +41,41 @@ const btnCheck = document.querySelector('[data-action="edit"]');
 
 // ✅ Не забывайте привязывать контекст методов объекта
 btnCheck.addEventListener("click", mango.showUsername.bind(mango));
+
+const form = document.querySelector(".register-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { username, password },
+  } = event.currentTarget;
+  console.log(username.value, password.value);
+});
+
+// pizza
+
+const select = document.querySelector(".pizza-select");
+const textOutput = document.querySelector(".text-output");
+const valueOutput = document.querySelector(".value-output");
+
+setOutput();
+
+select.addEventListener("change", setOutput);
+
+function setOutput() {
+  const selectedOptionValue = select.value;
+  const selectedOptionIndex = select.selectedIndex;
+  const selectedOptionText = select.options[selectedOptionIndex].text;
+
+  textOutput.textContent = selectedOptionText;
+  valueOutput.textContent = selectedOptionValue;
+}
+
+// input value
+
+const textInput = document.querySelector(".text-input");
+const output = document.querySelector(".output");
+
+textInput.addEventListener("input", (event) => {
+  output.textContent = event.currentTarget.value;
+});
