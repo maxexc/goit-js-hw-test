4da12303748a7bb576ccc7dcfb305aa742216d76
Maksym Solomko <max.exc7@gmail.com>
2022-09-20 15:36:36 +0300

    up

--- js/js-hw7.js
+++ js/js-hw7.js
@@ -79,3 +79,21 @@ const output = document.querySelector(".output");
 textInput.addEventListener("input", (event) => {
   output.textContent = event.currentTarget.value;
 });
+
+// ----event bubbling----
+
+const parent = document.querySelector("#parent");
+const child = document.querySelector("#child");
+const descendant = document.querySelector("#descendant");
+
+parent.addEventListener("click", () => {
+  alert("Parent click handler");
+});
+
+child.addEventListener("click", () => {
+  alert("Child click handler");
+});
+
+descendant.addEventListener("click", () => {
+  alert("Descendant click handler");
+});
